98bbd2c59e0e758843096b35f2857ee53a009d45
JunLangBai <1279477694@qq.com>
2025-10-28 20:14:00 +0800

    重做项目

---
+++ Assets/AQY/Scripts/乐器/AugumentPlayController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugumentPlayController : MonoBehaviour
+{
+    public SpriteRenderer voiceVisable;
+    public Collider triggerCollider;
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    
+    public void ControlVisable()
+    {
+        
+    }
+}
