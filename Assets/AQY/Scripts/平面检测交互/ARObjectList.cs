8efb245ab5a3ad4d1986af2b13de0a1b595bfcac
JunLangBai <1279477694@qq.com>
2026-07-20 15:24:34 +0800

    项目精简

--- Assets/AQY/Scripts/平面检测交互/ARObjectList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ARObj", menuName = "ARObj/ObjList")]
-public class ARObjectList : ScriptableObject
-{
-    public List<GameObject> objectList = new List<GameObject>();
-}
